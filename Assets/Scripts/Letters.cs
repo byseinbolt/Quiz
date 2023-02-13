c69e8d986e278464eaa7c2bb859b8db2918ac440
4rs69 <4rseniyFedorov@gmail.com>
2023-02-13 13:33:03 +0300

    add scripts "Letters" and sprite numbers 1-10

---
+++ Assets/Scripts/Letters.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "Letters",menuName = "Letter")]
+public class Letters : ScriptableObject
+{
+    [SerializeField]
+    private string _name;
+    
+    public string Name { get => _name; }
+
+    [SerializeField]
+    private Sprite _sprite;
+    
+    public Sprite Sprite { get => _sprite;}
+    
+
+}
