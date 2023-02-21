da528c1b3a2db45fd779cee4238504a12fdaa46f
4rs69 <4rseniyFedorov@gmail.com>
2023-02-21 11:40:20 +0300

    add script BackgroundSettingsCell.cs

---
+++ Assets/Scripts/BackgroundSettingsCell.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class BackgroundSettingsCell : MonoBehaviour
+{
+    [Header("Background settings")]
+    [SerializeField] private float _hueMin;
+    [SerializeField] private float _hueMax = 1;
+    [SerializeField] private float _saturationMin = 1;
+    [SerializeField] private float _saturationMax = 1;
+    [SerializeField] private float _valueMin = 1;
+    [SerializeField] private float _valueMax = 1;
+    [SerializeField] private float _alphaMin = 0.6f;
+    [SerializeField] private float _alphaMax = 0.6f;
+    
+    public Color CustomBackground()
+    {
+        return Random.ColorHSV(_hueMin, _hueMax, _saturationMin, 
+            _saturationMax, _valueMin, _valueMax, _alphaMin, _alphaMax);
+    }
+}
--- Assets/Scripts/Cell.cs
+++ Assets/Scripts/Cell.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using UnityEngine;
 using UnityEngine.UI;
-using Random = UnityEngine.Random;
 
 public class Cell : MonoBehaviour
 {
@@ -13,29 +12,20 @@ public class Cell : MonoBehaviour
     [SerializeField]
     private Image _background;
     
+    private BackgroundSettingsCell _backgroundCell;
     private Action<Cell> _onClicked;
-
-    [Header("Background settings")]
-    [SerializeField] private float _hueMin;
-    [SerializeField] private float _hueMax = 1;
-    [SerializeField] private float _saturationMin = 1;
-    [SerializeField] private float _saturationMax = 1;
-    [SerializeField] private float _valueMin = 1;
-    [SerializeField] private float _valueMax = 1;
-    [SerializeField] private float _alphaMin = 0.6f;
-    [SerializeField] private float _alphaMax = 0.6f;
     
     public void Initialize(Sprite gameItemView)
     {
         _image.sprite = gameItemView;
-        _background.color = Random.ColorHSV(_hueMin, _hueMax, _saturationMin, 
-            _saturationMax, _valueMin, _valueMax, _alphaMin, _alphaMax);
+        _background.color = _backgroundCell.CustomBackground();
     }
-
+    
     public void Click()
     {
         _onClicked?.Invoke(this);
     }
+    
     public void SetClickCallback(Action<Cell> onClicked)
     {
         _onClicked = onClicked;
